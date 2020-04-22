31d9710fafc58a8af2c2964c7c98c0af1110ae65
nehasharma2998 <nehasharma29@outlook.com>
2020-04-21 23:29:35 -0400

    vehicle with axis

--- vehicles/vehicle-query-2b.php
+++ vehicles/vehicle-query-2b.php
@@ -304,11 +304,14 @@ $chart_data = substr($chart_data, 0, -2);
             </form>
         </div>
 
+<div class="y-axis"><h2>y-axis</h2></div>
         <div class="display-full">
             <h1>Ratio of collisions between various different types of vehicles between <?=$start?> and <?=$end?>.</h1>
             <div id="chart"></div>
-            <div id="legend" class="bars-legend"></div>
+            <h2>x axis</h2>
+            
         </div>
+        <div id="legend" class="bars-legend display-full"></div>
 
     </div>
 
